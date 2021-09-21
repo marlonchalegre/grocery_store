09eaacacf985c585528dd691c3bafc226166969b
Marlon Chalegre <mcp@cesar.org.br>
2021-09-20 21:26:23 -0300

    Add tab navigation

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_store/page/parent_page.dart';
 import 'package:grocery_store/widget/home_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentScreen = 0;
+
+  final List<Widget> _screens = [HomeWidget(), MeuPai()];
 
   @override
   Widget build(BuildContext context) {
@@ -14,7 +24,38 @@ class HomePage extends StatelessWidget {
           child: Image.asset("assets/images/header_app_icon.png"),
         ),
       ),
-      body: HomeWidget(),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.amber,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business_center),
+            label: "Shop",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.manage_search_outlined),
+            label: "Explore",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline_outlined),
+            label: "Favorite",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_outlined),
+            label: "Account",
+          )
+        ],
+        currentIndex: _currentScreen,
+        onTap: (index) => {
+          setState(() {
+            _currentScreen = index;
+          })
+        },
+      ),
+      body: _screens[_currentScreen],
     );
   }
 }
